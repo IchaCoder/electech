67cc1ee767f58703fc094c3d555c60338dc1c5fe
Emmanuel Yeboah <ichabordyeboah247@gmail.com>
2024-07-14 18:45:48 +0000

    added hero section

---
+++ components/hero/index.tsx
@@ -0,0 +1,41 @@
+import { Button, Stack, Text } from "@chakra-ui/react";
+import React from "react";
+
+type Props = {};
+
+const Hero = (props: Props) => {
+	return (
+		<Stack maxW={"1000px"} mx={"auto"} gap={10} py={{ base: 12, lg: 20 }} px={4}>
+			<Stack gap={4}>
+				<Text
+					fontSize={{ base: "3xl", md: "5xl" }}
+					textAlign={"center"}
+					fontWeight={"bold"}
+				>
+					Giving You The Power Of Choice, <br /> Echoing Your Voice.
+				</Text>
+				<Text color={"rgba(0,0,0,.8)"} textAlign={"center"}>
+					Your number one electronic voting system
+				</Text>
+			</Stack>
+			<Stack gap={4}>
+				<Text color={"rgba(0,0,0,.8)"} textAlign={"center"}>
+					Are you an event organizer?
+				</Text>
+				<Button
+					rounded={"full"}
+					w={"max-content"}
+					size={"lg"}
+					mx={"auto"}
+					bgColor={"brand.primary"}
+					color={"white"}
+					_hover={{ opacity: 0.7 }}
+				>
+					Get Started
+				</Button>
+			</Stack>
+		</Stack>
+	);
+};
+
+export default Hero;
