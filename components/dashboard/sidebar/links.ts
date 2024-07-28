7cd780446321f54fd6ea1b99b3fa9dbbf75e176b
Emmanuel Yeboah <ichabordyeboah247@gmail.com>
2024-07-28 13:00:21 +0000

    added results nav link to sidebar

--- components/dashboard/sidebar/links.ts
+++ components/dashboard/sidebar/links.ts
@@ -1,11 +1,17 @@
 import { IoHomeOutline, IoPersonOutline, IoSettingsOutline } from "react-icons/io5";
 import { BiCategory } from "react-icons/bi";
+import { FaChartLine } from "react-icons/fa6";
 
 export const nav_links = [
   {
     name: "Home",
     icon: IoHomeOutline,
     href: "/dashboard",
+  },
+  {
+    name: "Results",
+    icon: FaChartLine,
+    href: "/dashboard/results",
   },
   {
     name: "Cateogries",
