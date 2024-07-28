5a6195021fa17d58475bcee1a31fd900b513fb08
Emmanuel Yeboah <ichabordyeboah247@gmail.com>
2024-07-28 13:00:51 +0000

    added start date and due date

--- components/dashboard/home/index.tsx
+++ components/dashboard/home/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Stack, Text } from "@chakra-ui/react";
 import React from "react";
 import CountDown from "./count-down-timer/count-down";
 import { Stats } from "./stats";
@@ -10,8 +10,22 @@ const Home = (props: Props) => {
   return (
     <Box py={8} px={{ base: 0, sm: 4, xl: 12 }}>
       <Text fontWeight={"bold"} fontSize={{ base: "xl", lg: "3xl" }}>
-        UG SRC General Elections
+        UG SRC General Elections - 2024
       </Text>
+      <Stack flexDir={"row"} gap={10}>
+        <Stack>
+          <Text fontSize={"lg"} fontWeight={"bold"}>
+            Start Date
+          </Text>
+          <Text>9th June 2024 - 09:00 AM</Text>
+        </Stack>
+        <Stack>
+          <Text fontSize={"lg"} fontWeight={"bold"}>
+            Due Date
+          </Text>
+          <Text>9th June 2024 - 07:00 AM</Text>
+        </Stack>
+      </Stack>
       <CountDown />
       <Stats />
     </Box>
