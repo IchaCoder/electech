92047b1ce00b3da00da497a09033e8d41af90ad6
Emmanuel Yeboah <ichabordyeboah247@gmail.com>
2024-08-14 02:17:28 +0000

    fixed build error

--- components/nav/PopoverIcon.tsx
+++ components/nav/PopoverIcon.tsx
@@ -2,11 +2,15 @@ import { Icon } from "@chakra-ui/react";
 import * as React from "react";
 import { FiChevronDown } from "react-icons/fi";
 
-export const PopoverIcon = ({ isOpen }) => {
-	const iconStyles = {
-		transform: isOpen ? "rotate(-180deg)" : undefined,
-		transition: "transform 0.2s",
-		transformOrigin: "center",
-	};
-	return <Icon aria-hidden as={FiChevronDown} __css={iconStyles} />;
+type PopoverIconProps = {
+  isOpen: boolean;
+};
+
+export const PopoverIcon = ({ isOpen }: PopoverIconProps) => {
+  const iconStyles = {
+    transform: isOpen ? "rotate(-180deg)" : undefined,
+    transition: "transform 0.2s",
+    transformOrigin: "center",
+  };
+  return <Icon aria-hidden as={FiChevronDown} __css={iconStyles} />;
 };
