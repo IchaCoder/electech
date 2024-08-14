4d61429f957cfb0b1597dea8afa560dc1474406c
Emmanuel Yeboah <ichabordyeboah247@gmail.com>
2024-08-14 02:13:11 +0000

    installed chadcn

--- components/dashboard/category/index.tsx
+++ components/dashboard/category/index.tsx
@@ -1,108 +1,195 @@
-import { Avatar, Box, Button, ButtonGroup, SimpleGrid, Stack, Text } from "@chakra-ui/react";
+"use client";
+import {
+  Avatar,
+  Box,
+  Button,
+  ButtonGroup,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  SimpleGrid,
+  Stack,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import { MdOutlineEdit } from "react-icons/md";
+import { IoMdAdd } from "react-icons/io";
 import Link from "next/link";
+import { FiEdit2, FiMoreVertical } from "react-icons/fi";
+import DeleteParticipantDialog from "./modal/delete-participant";
+import EditParticipant from "./modal/edit";
+import DeleteCategoryDialog from "./modal/remove-category";
 
 type Props = {};
 
 const Category = (props: Props) => {
+  const {
+    isOpen: isOpenDeleteParticipant,
+    onOpen: onOpenDeleteParticipant,
+    onClose: onCloseDeleteParticipant,
+  } = useDisclosure();
+
+  const {
+    isOpen: isOpenEditParticipant,
+    onOpen: onOpenEditParticipant,
+    onClose: onCloseEditParticipant,
+  } = useDisclosure();
+
+  const {
+    isOpen: isOpenRemoveCategpry,
+    onOpen: onOpenRemoveCategpry,
+    onClose: onCloseRemoveCategpry,
+  } = useDisclosure();
+
   return (
-    <Box py={8} px={{ base: 0, sm: 4, xl: 12 }}>
-      <Text fontWeight={"bold"} fontSize={{ base: "xl", md: "2xl", lg: "4xl" }}>
-        Categories
-      </Text>
-      <Stack mt={8} gap={8}>
-        <Stack bgColor={"rgba(155, 214, 232, 0.5)"} py={8} gap={8} px={{ base: 2, sm: 4, xl: 12 }} rounded={"xl"}>
-          <Stack flexDir={"row"} justifyContent={"space-between"}>
-            <Text fontSize={"lg"} fontWeight={"medium"} textAlign={"center"}>
-              SRC President
-            </Text>
-            <ButtonGroup variant="solid" spacing={{ base: 2, md: "6" }} justifyContent={"center"}>
-              <Button size={{ base: "xs", md: "sm" }} colorScheme="blue" rounded={"sm"} leftIcon={<MdOutlineEdit />}>
-                Edit
-              </Button>
-              <Button size={{ base: "xs", md: "sm" }} colorScheme="red" rounded={"sm"} leftIcon={<RiDeleteBin6Line />}>
-                Delete
-              </Button>
-            </ButtonGroup>
-          </Stack>
-          <Box>
-            <SimpleGrid columns={{ base: 2, sm: 3, lg: 5 }} gap={{ base: "4", md: "6" }} justifyItems={"center"}>
-              <Box width={"max-content"}>
-                <Avatar name="Dan Abramov" size={{ base: "2xl", md: "xl" }} src="https://bit.ly/dan-abramov" />
-                <Text fontSize={"15px"} mt={1} textAlign={"center"}>
-                  Dan Abramov
-                </Text>
-              </Box>
-              <Box width={"max-content"}>
-                <Avatar name="Dan Abramov" size={{ base: "2xl", md: "xl" }} src="https://bit.ly/dan-abramov" />
-                <Text fontSize={"15px"} mt={1} textAlign={"center"}>
-                  Dan Abramov
-                </Text>
-              </Box>
-              <Box width={"max-content"}>
-                <Avatar name="Dan Abramov" size={{ base: "2xl", md: "xl" }} src="https://bit.ly/dan-abramov" />
-                <Text fontSize={"15px"} mt={1} textAlign={"center"}>
-                  Dan Abramov
-                </Text>
-              </Box>
-            </SimpleGrid>
-          </Box>
+    <>
+      {isOpenDeleteParticipant && (
+        <DeleteParticipantDialog isOpen={isOpenDeleteParticipant} onClose={onCloseDeleteParticipant} />
+      )}
+      {isOpenEditParticipant && <EditParticipant isOpen={isOpenEditParticipant} onClose={onCloseEditParticipant} />}
+      {isOpenRemoveCategpry && <DeleteCategoryDialog isOpen={isOpenRemoveCategpry} onClose={onCloseRemoveCategpry} />}
+      <Box py={8} px={{ base: 0, sm: 4, xl: 12 }}>
+        <Stack flexDir={"row"} justifyContent={"space-between"} alignItems={"center"}>
+          <Text fontWeight={"bold"} fontSize={{ base: "xl", md: "2xl", lg: "4xl" }}>
+            Categories
+          </Text>
+          <Button
+            as={Link}
+            href={"/dashboard/add-category"}
+            bgColor={"rgba(97, 153, 203, 1)"}
+            color={"white"}
+            _hover={{ opacity: 0.7 }}
+            _focus={{ opacity: 0.7 }}
+          >
+            Add Category
+          </Button>
         </Stack>
-        <Stack bgColor={"rgba(155, 214, 232, 0.5)"} py={8} gap={8} px={{ base: 2, sm: 4, xl: 12 }} rounded={"xl"}>
-          <Stack flexDir={"row"} justifyContent={"space-between"}>
-            <Text fontSize={"lg"} fontWeight={"medium"} textAlign={"center"}>
-              SRC Secretary
-            </Text>
-            <ButtonGroup variant="solid" spacing={{ base: 2, md: "6" }} justifyContent={"center"}>
-              <Button size={{ base: "xs", md: "sm" }} colorScheme="blue" rounded={"sm"} leftIcon={<MdOutlineEdit />}>
-                Edit
-              </Button>
-              <Button size={{ base: "xs", md: "sm" }} colorScheme="red" rounded={"sm"} leftIcon={<RiDeleteBin6Line />}>
-                Delete
-              </Button>
-            </ButtonGroup>
+        <Stack mt={8} gap={8}>
+          <Stack bgColor={"rgba(155, 214, 232, 0.5)"} py={8} gap={8} px={{ base: 2, sm: 4, xl: 12 }} rounded={"xl"}>
+            <Stack flexDir={"row"} justifyContent={"space-between"}>
+              <Text fontSize={"lg"} fontWeight={"medium"} textAlign={"center"}>
+                SRC President
+              </Text>
+              <ButtonGroup variant="solid" spacing={{ base: 2, md: "6" }} justifyContent={"center"}>
+                <Button
+                  size={{ base: "xs", md: "sm" }}
+                  colorScheme="blue"
+                  rounded={"sm"}
+                  leftIcon={<IoMdAdd />}
+                  title="Add another participant"
+                >
+                  Add
+                </Button>
+                <Button
+                  size={{ base: "xs", md: "sm" }}
+                  colorScheme="red"
+                  rounded={"sm"}
+                  leftIcon={<RiDeleteBin6Line />}
+                  title="Remove this category"
+                  onClick={onOpenRemoveCategpry}
+                >
+                  Remove
+                </Button>
+              </ButtonGroup>
+            </Stack>
+            <Box>
+              <SimpleGrid columns={{ base: 2, sm: 3, lg: 5 }} gap={{ base: "4", md: "6" }} justifyItems={"center"}>
+                <Box width={"max-content"} position={"relative"}>
+                  <Menu>
+                    <MenuButton
+                      as={IconButton}
+                      aria-label="Options"
+                      position={"absolute"}
+                      right={0}
+                      zIndex={1}
+                      width={"1.5rem"}
+                      height={"2rem"}
+                      minW={"0"}
+                      border={"none"}
+                      bgColor={"gray.50"}
+                      icon={<FiMoreVertical />}
+                      variant="outline"
+                    />
+                    <MenuList>
+                      <MenuItem icon={<FiEdit2 />} onClick={onOpenEditParticipant}>
+                        Edit
+                      </MenuItem>
+                      <MenuItem icon={<RiDeleteBin6Line />} onClick={onOpenDeleteParticipant}>
+                        Delete
+                      </MenuItem>
+                    </MenuList>
+                  </Menu>
+                  <Avatar name="Dan Abramov" size={{ base: "2xl", md: "xl" }} src="https://bit.ly/dan-abramov" />
+                  <Text fontSize={"15px"} mt={1} textAlign={"center"}>
+                    Dan Abramov
+                  </Text>
+                </Box>
+              </SimpleGrid>
+            </Box>
           </Stack>
-          <Box>
-            <SimpleGrid columns={{ base: 2, sm: 3, lg: 5 }} gap={{ base: "4", md: "6" }} justifyItems={"center"}>
-              <Box width={"max-content"}>
-                <Avatar name="Dan Abramov" size={{ base: "2xl", md: "xl" }} src="https://bit.ly/dan-abramov" />
-                <Text fontSize={"15px"} mt={1} textAlign={"center"}>
-                  Dan Abramov
-                </Text>
-              </Box>
-              <Box width={"max-content"}>
-                <Avatar name="Dan Abramov" size={{ base: "2xl", md: "xl" }} src="https://bit.ly/dan-abramov" />
-                <Text fontSize={"15px"} mt={1} textAlign={"center"}>
-                  Dan Abramov
-                </Text>
-              </Box>
-              <Box width={"max-content"}>
-                <Avatar name="Dan Abramov" size={{ base: "2xl", md: "xl" }} src="https://bit.ly/dan-abramov" />
-                <Text fontSize={"15px"} mt={1} textAlign={"center"}>
-                  Dan Abramov
-                </Text>
-              </Box>
-            </SimpleGrid>
-          </Box>
+          <Stack bgColor={"rgba(155, 214, 232, 0.5)"} py={8} gap={8} px={{ base: 2, sm: 4, xl: 12 }} rounded={"xl"}>
+            <Stack flexDir={"row"} justifyContent={"space-between"}>
+              <Text fontSize={"lg"} fontWeight={"medium"} textAlign={"center"}>
+                SRC Secretary
+              </Text>
+              <ButtonGroup variant="solid" spacing={{ base: 2, md: "6" }} justifyContent={"center"}>
+                <Button size={{ base: "xs", md: "sm" }} colorScheme="blue" rounded={"sm"} leftIcon={<MdOutlineEdit />}>
+                  Edit
+                </Button>
+                <Button
+                  size={{ base: "xs", md: "sm" }}
+                  colorScheme="red"
+                  rounded={"sm"}
+                  leftIcon={<RiDeleteBin6Line />}
+                >
+                  Delete
+                </Button>
+              </ButtonGroup>
+            </Stack>
+            <Box>
+              <SimpleGrid columns={{ base: 2, sm: 3, lg: 5 }} gap={{ base: "4", md: "6" }} justifyItems={"center"}>
+                <Box width={"max-content"}>
+                  <Avatar name="Dan Abramov" size={{ base: "2xl", md: "xl" }} src="https://bit.ly/dan-abramov" />
+                  <Text fontSize={"15px"} mt={1} textAlign={"center"}>
+                    Dan Abramov
+                  </Text>
+                </Box>
+                <Box width={"max-content"}>
+                  <Avatar name="Dan Abramov" size={{ base: "2xl", md: "xl" }} src="https://bit.ly/dan-abramov" />
+                  <Text fontSize={"15px"} mt={1} textAlign={"center"}>
+                    Dan Abramov
+                  </Text>
+                </Box>
+                <Box width={"max-content"}>
+                  <Avatar name="Dan Abramov" size={{ base: "2xl", md: "xl" }} src="https://bit.ly/dan-abramov" />
+                  <Text fontSize={"15px"} mt={1} textAlign={"center"}>
+                    Dan Abramov
+                  </Text>
+                </Box>
+              </SimpleGrid>
+            </Box>
+          </Stack>
         </Stack>
-      </Stack>
-      <Box textAlign="center" mt={8}>
-        <Text fontSize={{ base: "xl", xl: "2xl" }} fontWeight={"bold"}>
-          No categories found
-        </Text>
-        <Button
-          as={Link}
-          href={"/dashboard/add-category"}
-          bgColor={"rgba(97, 153, 203, 1)"}
-          color={"white"}
-          _hover={{ opacity: 0.7 }}
-          _focus={{ opacity: 0.7 }}
-        >
-          Add Category
-        </Button>
+        <Box textAlign="center" mt={8}>
+          <Text fontSize={{ base: "xl", xl: "2xl" }} fontWeight={"bold"}>
+            No categories found
+          </Text>
+          <Button
+            as={Link}
+            href={"/dashboard/add-category"}
+            bgColor={"rgba(97, 153, 203, 1)"}
+            color={"white"}
+            _hover={{ opacity: 0.7 }}
+            _focus={{ opacity: 0.7 }}
+          >
+            Add Category
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
